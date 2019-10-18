d7725595f9273c780a7bf8d0ee9cc2a8dbe7b6f3
Trinh Dung <c170017@e.ntu.edu.sg>
2019-10-18 19:40:46 +0800

    NoteListViewModel

--- android/app/src/main/java/com/android/example/reemy/activities/NoteList/NoteListViewModel.kt
+++ android/app/src/main/java/com/android/example/reemy/activities/NoteList/NoteListViewModel.kt
@@ -1,2 +1,31 @@
 package com.android.example.reemy.activities.NoteList
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.android.example.reemy.utils.AllEvents
+import com.applandeo.materialcalendarview.EventDay
+import kotlinx.coroutines.*
+
+class NoteListViewModel(application: Application): AndroidViewModel(application){
+    private var mEventDays = MutableLiveData<MutableList<EventDay>>()
+    private var viewModelJob : Job = Job()
+    private var uiScope : CoroutineScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+    init {
+        loadEventDays()
+    }
+
+    private fun loadEventDays() {
+        uiScope.launch{
+            mEventDays.value = getEventsFromDatabase()
+        }
+    }
+
+    private suspend fun getEventsFromDatabase(): MutableList<EventDay>? {
+        return withContext(Dispatchers.IO){
+            var events = AllEvents.mEventDays
+            events
+        }
+    }
+
+}
