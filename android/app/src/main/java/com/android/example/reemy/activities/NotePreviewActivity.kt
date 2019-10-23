1d90ec438163ac44202d517588ac7dbabfb05e2c
Trinh Dung <c170017@e.ntu.edu.sg>
2019-10-24 01:45:57 +0800

    Implement simple delete event method

--- android/app/src/main/java/com/android/example/reemy/activities/NotePreviewActivity.kt
+++ android/app/src/main/java/com/android/example/reemy/activities/NotePreviewActivity.kt
@@ -1,10 +1,13 @@
 package com.android.example.reemy.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Parcelable
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.android.example.reemy.databinding.ActivityNotePreviewBinding
+import com.android.example.reemy.utils.AllEvents
 import com.android.example.reemy.utils.MyEventDay
 import com.applandeo.materialcalendarview.EventDay
 import java.text.SimpleDateFormat
@@ -12,13 +15,19 @@ import java.util.*
 
 class NotePreviewActivity: AppCompatActivity() {
     private lateinit var binding: ActivityNotePreviewBinding
-
+    private lateinit var mEvent: EventDay
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, com.android.example.reemy.R.layout.activity_note_preview)
 
+        binding.deleteEventButton.setOnClickListener{
+            it?.let{
+                deleteEvent()
+            }
+        }
         //get intent
         val mIntent = intent
+
 
         mIntent?.let {
             val event : Parcelable = mIntent.getParcelableExtra(MainActivity.EVENT)
@@ -27,7 +36,7 @@ class NotePreviewActivity: AppCompatActivity() {
                 val myEventDay: MyEventDay = event as MyEventDay
                 supportActionBar!!.title = getFormattedDate(myEventDay.calendar.time)
                 binding.note.text = myEventDay.getNote()
-
+                mEvent = event
                 return
             }
 
@@ -36,7 +45,14 @@ class NotePreviewActivity: AppCompatActivity() {
                 supportActionBar!!.title = getFormattedDate(eventDay.calendar.time)
             }
         }
+    }
 
+    private fun deleteEvent() {
+        mEvent?.let{
+            AllEvents.mEventDays.remove(mEvent)
+        }
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 
 
