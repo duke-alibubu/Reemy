d176e2bbc3b1c85c64f204fe79dd584fdfb0c635
Trinh Dung <c170017@e.ntu.edu.sg>
2019-11-30 17:54:47 +0800

    Use a JpaRepository is simple enough

---
+++ test_backend/reemy-solo-webservices/src/main/java/com/alibubu/reemy/solo/reemysolowebservices/entity/EventNoteRepository.java
@@ -0,0 +1,6 @@
+package com.alibubu.reemy.solo.reemysolowebservices.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventNoteRepository extends JpaRepository<EventNote, Long> {
+}
